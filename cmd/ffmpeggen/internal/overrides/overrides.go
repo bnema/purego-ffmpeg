b7035ceeb5f9d2ede6a057f7e0aa1ca55cf4b0ee
brice <b@bnema.dev>
2026-03-26 07:20:29 +0100

    feat: add Go-typed overrides for domain grouping

---
+++ cmd/ffmpeggen/internal/overrides/overrides.go
@@ -0,0 +1,194 @@
+// cmd/ffmpeggen/internal/overrides/overrides.go
+package overrides
+
+// Domain groups C functions into an outbound port interface and public wrapper type.
+type Domain struct {
+	Name          string     // "format", "codec", etc.
+	Library       string     // "libavformat", "libavcodec", etc.
+	PortInterface string     // Go interface name: "FormatCAPI"
+	PublicType    string     // Go public type: "FormatContext" (empty = free functions only)
+	Functions     []FuncMap  // C→Go name mappings
+	Accessors     []Accessor // offset-based struct field accessors
+	Enums         []string   // enum names to include in this domain
+}
+
+type FuncMap struct {
+	C  string // C function name: "avformat_open_input"
+	Go string // Go method name: "OpenInput"
+}
+
+type Accessor struct {
+	Struct string // C struct name: "AVCodecContext"
+	Field  string // C field name: "width"
+	GoName string // Go accessor name: "Width"
+	Type   string // Go type: "int32"
+	Offset int    // byte offset in struct
+}
+
+// Override controls per-function generation behavior.
+type Override struct {
+	Name string
+	Skip bool // skip generation entirely
+}
+
+var Overrides = []Override{
+	{Name: "av_err2str", Skip: true},
+	{Name: "av_ts2str", Skip: true},
+	{Name: "av_ts2timestr", Skip: true},
+	{Name: "av_fourcc2str", Skip: true},
+}
+
+var Domains = []Domain{
+	{
+		Name: "format", Library: "libavformat",
+		PortInterface: "FormatCAPI", PublicType: "FormatContext",
+		Functions: []FuncMap{
+			{C: "avformat_alloc_context", Go: "AllocContext"},
+			{C: "avformat_free_context", Go: "FreeContext"},
+			{C: "avformat_open_input", Go: "OpenInput"},
+			{C: "avformat_find_stream_info", Go: "FindStreamInfo"},
+			{C: "av_read_frame", Go: "ReadFrame"},
+			{C: "avformat_close_input", Go: "CloseInput"},
+			{C: "avformat_alloc_output_context2", Go: "AllocOutputContext2"},
+			{C: "avformat_new_stream", Go: "NewStream"},
+			{C: "avformat_write_header", Go: "WriteHeader"},
+			{C: "av_interleaved_write_frame", Go: "InterleavedWriteFrame"},
+			{C: "av_write_trailer", Go: "WriteTrailer"},
+		},
+		Accessors: []Accessor{
+			{Struct: "AVFormatContext", Field: "nb_streams", GoName: "NbStreams", Type: "uint32", Offset: 44},
+			{Struct: "AVStream", Field: "index", GoName: "Index", Type: "int32", Offset: 0},
+			{Struct: "AVStream", Field: "codecpar", GoName: "CodecParameters", Type: "unsafe.Pointer", Offset: 8},
+			{Struct: "AVStream", Field: "time_base", GoName: "TimeBase", Type: "AVRational", Offset: 16},
+		},
+	},
+	{
+		Name: "codec", Library: "libavcodec",
+		PortInterface: "CodecCAPI", PublicType: "CodecContext",
+		Functions: []FuncMap{
+			{C: "avcodec_find_decoder", Go: "FindDecoder"},
+			{C: "avcodec_find_encoder", Go: "FindEncoder"},
+			{C: "avcodec_find_decoder_by_name", Go: "FindDecoderByName"},
+			{C: "avcodec_find_encoder_by_name", Go: "FindEncoderByName"},
+			{C: "avcodec_alloc_context3", Go: "AllocContext3"},
+			{C: "avcodec_free_context", Go: "FreeContext"},
+			{C: "avcodec_open2", Go: "Open2"},
+			{C: "avcodec_send_packet", Go: "SendPacket"},
+			{C: "avcodec_receive_frame", Go: "ReceiveFrame"},
+			{C: "avcodec_send_frame", Go: "SendFrame"},
+			{C: "avcodec_receive_packet", Go: "ReceivePacket"},
+			{C: "avcodec_parameters_to_context", Go: "ParametersToContext"},
+			{C: "avcodec_parameters_from_context", Go: "ParametersFromContext"},
+		},
+		Accessors: []Accessor{
+			{Struct: "AVCodecContext", Field: "width", GoName: "Width", Type: "int32", Offset: 112},
+			{Struct: "AVCodecContext", Field: "height", GoName: "Height", Type: "int32", Offset: 116},
+			{Struct: "AVCodecContext", Field: "pix_fmt", GoName: "PixelFormat", Type: "int32", Offset: 80},
+			{Struct: "AVCodecContext", Field: "sample_rate", GoName: "SampleRate", Type: "int32", Offset: 156},
+			{Struct: "AVCodecContext", Field: "time_base", GoName: "TimeBase", Type: "AVRational", Offset: 40},
+		},
+		Enums: []string{"AVCodecID"},
+	},
+	{
+		Name: "packet", Library: "libavcodec",
+		PortInterface: "PacketCAPI", PublicType: "Packet",
+		Functions: []FuncMap{
+			{C: "av_packet_alloc", Go: "Alloc"},
+			{C: "av_packet_free", Go: "Free"},
+			{C: "av_packet_unref", Go: "Unref"},
+			{C: "av_packet_ref", Go: "Ref"},
+			{C: "av_packet_rescale_ts", Go: "RescaleTs"},
+		},
+	},
+	{
+		Name: "frame", Library: "libavutil",
+		PortInterface: "FrameCAPI", PublicType: "Frame",
+		Functions: []FuncMap{
+			{C: "av_frame_alloc", Go: "Alloc"},
+			{C: "av_frame_free", Go: "Free"},
+			{C: "av_frame_unref", Go: "Unref"},
+			{C: "av_frame_ref", Go: "Ref"},
+			{C: "av_frame_clone", Go: "Clone"},
+			{C: "av_frame_get_buffer", Go: "GetBuffer"},
+			{C: "av_frame_is_writable", Go: "IsWritable"},
+			{C: "av_frame_make_writable", Go: "MakeWritable"},
+		},
+	},
+	{
+		Name: "swscale", Library: "libswscale",
+		PortInterface: "SwscaleCAPI", PublicType: "SwscaleContext",
+		Functions: []FuncMap{
+			{C: "sws_getContext", Go: "GetContext"},
+			{C: "sws_scale", Go: "Scale"},
+			{C: "sws_freeContext", Go: "FreeContext"},
+		},
+	},
+	{
+		Name: "swresample", Library: "libswresample",
+		PortInterface: "SwresampleCAPI", PublicType: "SwresampleContext",
+		Functions: []FuncMap{
+			{C: "swr_alloc", Go: "Alloc"},
+			{C: "swr_init", Go: "Init"},
+			{C: "swr_convert", Go: "Convert"},
+			{C: "swr_free", Go: "Free"},
+		},
+	},
+	{
+		Name: "dict", Library: "libavutil",
+		PortInterface: "DictCAPI", PublicType: "Dictionary",
+		Functions: []FuncMap{
+			{C: "av_dict_get", Go: "Get"},
+			{C: "av_dict_set", Go: "Set"},
+			{C: "av_dict_free", Go: "Free"},
+			{C: "av_dict_count", Go: "Count"},
+		},
+	},
+	{
+		Name: "util", Library: "libavutil",
+		PortInterface: "UtilCAPI", PublicType: "", // no wrapper type, free functions only
+		Functions: []FuncMap{
+			{C: "av_malloc", Go: "Malloc"},
+			{C: "av_free", Go: "Free"},
+			{C: "av_opt_set", Go: "OptSet"},
+			{C: "av_opt_set_int", Go: "OptSetInt"},
+			{C: "av_log_set_level", Go: "LogSetLevel"},
+			{C: "av_log_get_level", Go: "LogGetLevel"},
+			{C: "av_rescale_q", Go: "RescaleQ"},
+			{C: "av_image_get_buffer_size", Go: "ImageGetBufferSize"},
+			{C: "av_strerror", Go: "Strerror"},
+		},
+	},
+}
+
+// TypeMap defines value types and enums shared across domains.
+var Enums = []EnumDef{
+	{C: "AVPixelFormat", Go: "PixelFormat"},
+	{C: "AVSampleFormat", Go: "SampleFormat"},
+	{C: "AVMediaType", Go: "MediaType"},
+	{C: "AVCodecID", Go: "CodecID"},
+}
+
+type EnumDef struct {
+	C  string
+	Go string
+}
+
+var Structs = []StructDef{
+	{C: "AVRational", Go: "AVRational", Fields: []StructField{
+		{Name: "Num", Type: "int32"}, {Name: "Den", Type: "int32"},
+	}},
+	{C: "AVDictionaryEntry", Go: "DictionaryEntry", Fields: []StructField{
+		{Name: "Key", Type: "*byte"}, {Name: "Value", Type: "*byte"},
+	}},
+}
+
+type StructDef struct {
+	C      string
+	Go     string
+	Fields []StructField
+}
+
+type StructField struct {
+	Name string
+	Type string
+}
